5349fc11a16fa4339a4fe582655b64b24711645e
Mikalai Zarazaka <golmtz@gmail.com>
2022-05-31 11:58:06 +0300

    first commit

---
+++ index.ts
@@ -0,0 +1,6 @@
+const gameBoard = <HTMLCanvasElement>document.querySelector('#gameBoard')
+const ctx = gameBoard.getContext('2d')
+const scoreText = document.querySelector('#scoreText')
+const resetBtn = document.querySelector('#resetBtn')
+const gameWidth = gameBoard.width
+const gameHeight = gameBoard.height
